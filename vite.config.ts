1ba433526fca30caa83d70bb993403870e08e9fb
HEctor506 <164930438+HEctor506@users.noreply.github.com>
2026-06-01 22:33:59 +0000

    listoooo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dashboard-clima/",
+  base: "/dashboard/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
